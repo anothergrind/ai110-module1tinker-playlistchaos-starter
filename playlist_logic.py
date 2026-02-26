7cc430ea50c239f98306f75de3a3707567635a6f
anothergrind <ozorjikj@gmail.com>
2026-02-25 21:47:03 -0600

    fix: adding mixed songs to the lucky pick

--- playlist_logic.py
+++ playlist_logic.py
@@ -190,9 +190,14 @@ def lucky_pick(
         songs = playlists.get("Hype", [])
     elif mode == "chill":
         songs = playlists.get("Chill", [])
-    else:
-        songs = playlists.get("Hype", []) + playlists.get("Chill", [])
-
+    elif mode == "mixed":
+        songs = playlists.get("Mixed", [])
+    else:  # any
+        songs = (
+            playlists.get("Hype", [])
+            + playlists.get("Chill", [])
+            + playlists.get("Mixed", [])
+        )
     return random_choice_or_none(songs)
 
 
